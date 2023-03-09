ed186a2a774381645cfbfd49fdccb8e9b52309a6
Matt <mwbertolucci@gmail.com>
2023-03-09 16:02:46 -0300

    Improve UX & add comments for dev readability

--- src/modules/fetchingData.js
+++ src/modules/fetchingData.js
@@ -1,20 +1,24 @@
+// In this file we define the async functions necessary to consume the API
+
+// getScores will take the url from our specific game endpoint
 const getScores = async (url) => {
   try {
+    // we wait for the fetch promise response
     const response = await fetch(url);
-    if (!response.ok) {
-      return new Error(response.statusText);
-    }
-
+    // then we store the json() result in a data variable
     const data = await response.json();
 
     return data;
   } catch (error) {
+    // here we catch an error in case there's no data
     return error;
   }
 };
 
+// postScore will send the user score to the provided API endpoint url
 const postScore = async (url, userData) => {
   try {
+    // we wait for the fetch promise response
     const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify(userData),
@@ -22,15 +26,12 @@ const postScore = async (url, userData) => {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
-
-    if (!response.ok) {
-      return new Error(response.statusText);
-    }
-
+    // then we store the json() result in a data variable
     const data = await response.json();
 
     return data;
   } catch (error) {
+    // here we catch an error in case there's no data
     return error;
   }
 };
--- src/modules/handlingData.js
+++ src/modules/handlingData.js
@@ -1,10 +1,16 @@
+// In this file we handle the data provided by the API and the user
 import { getScores, postScore } from './fetchingData.js';
 
+// We store our game's url in a variable
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SzxB0D5b3hJBN8T3JMNC/scores/';
 
+// refreshData will render the content of our leaderboard
 const refreshData = () => {
   const scoresContainer = document.querySelector('.scores');
+  
+  // First we empty the container to avoid duplicated content
   scoresContainer.innerHTML = '';
+  // Then we inject the data returned by getScores 
   getScores(url).then((data) => {
     data.result.forEach((score) => {
       scoresContainer.insertAdjacentHTML('beforeend',
@@ -13,13 +19,20 @@ const refreshData = () => {
   });
 };
 
+// sendData will handle the user form submission
 const sendData = () => {
+  // First we store the user provided data in an object
   const data = {
     user: document.getElementById('form-name').value,
     score: document.getElementById('form-score').value,
   };
 
+  // Then we send the data to our game's API endpoint
   postScore(url, data);
+
+  // We make sure to empty the form's value to improve the user UX experience
+  document.getElementById('form-name').value = '';
+  document.getElementById('form-score').value = '';
 };
 
 export { refreshData, sendData };
